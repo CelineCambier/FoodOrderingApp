aadea79ebd70f0ca9581532bd8a1f12bfeb95bff
CelineCambier <cambier.celine2@gmail.com>
2024-03-28 21:34:32 -0700

    Image rendering and styling

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../../constants/Colors';
 import products from '@/assets/data/products';
 
-const product = products[0];
+const product = products[1];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image source={{uri: product.image}} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -15,7 +16,13 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
   title: {
     fontSize: 18,
