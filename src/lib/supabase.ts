ed290c60461ac77b83e3bea75b1b6fec0bd66eb0
CelineCambier <cambier.celine2@gmail.com>
2024-04-21 21:09:49 -0700

    Config supabase file

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://sfkuppoxgqeglsmbmxay.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNma3VwcG94Z3FlZ2xzbWJteGF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3NTg2MTMsImV4cCI6MjAyOTMzNDYxM30.0YklvLVP7rhNHSuLIblwyVo4Emb47ru2CYS3zDiQFjU";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
